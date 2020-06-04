4362542c7339e8524c68888bbf9f6d5f669a09b5
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-06-04 02:07:58 -0400

    add collections controller create action

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,17 @@
 class CollectionsController < ApplicationController
+  def create
+    # byebug
+    collection = Collection.find_or_create_by(collection_params)
+    if collection
+      render json: collection
+    else
+      render json: { message: 'Could not create collection' }
+    end
+  end
+
+  private 
+
+  def collection_params
+    params.require(:collection).permit(:user_id, :article_id)
+  end
 end
