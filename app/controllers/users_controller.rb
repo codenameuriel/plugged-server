819275db362af4fbed2a098612bf8c600c8cca7f
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-06-03 23:53:26 -0400

    add login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
+  def login
+    # byebug
+    user = User.find_by(username: params[:username])
+    if user 
+      render json: user
+    else
+      render json: { message: 'No user found by provided username'}
+    end
+  end
+
   def create
     # byebug
     user = User.find_or_create_by(user_params)
     if user
       render json: user
     else
-      render json: { message: 'No user found by provided username' }
+      render json: { message: 'Could not create user' }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :articles
   resources :collections
+
+  get '/users/login/:username', to: 'users#login'
 end
