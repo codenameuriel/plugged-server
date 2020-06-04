97376cab81391a1e3e0824c3c89a85acceb06aaf
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-06-04 01:34:31 -0400

    add create action in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,17 @@
 class ArticlesController < ApplicationController
+  def create
+    # byebug
+    article = Article.create(article_params)
+    if article 
+      render json: article
+    else
+      render json: { message: 'Could not create article' }
+    end
+  end
+
+  private 
+
+  def article_params
+    params.require(:article).permit(:author, :content, :description, :published_at, :source, :title, :url, :url_to_image)
+  end
 end
