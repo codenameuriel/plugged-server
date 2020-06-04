9425bf73895ea468ebd013605b0c393b81072d0d
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-06-03 22:40:48 -0400

    create db and migrate tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def create
+    byebug
+    user = User.create(user_params)
+    if user
+      render json: user
+    else
+      render json: { message: 'No user found by provided username' }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
