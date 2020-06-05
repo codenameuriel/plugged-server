0e0d1f1ca565626e3767359c703a315a7f01765b
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-06-05 11:31:48 -0400

    add logic to render users collection articles

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,19 @@
 class CollectionsController < ApplicationController
+  def show
+    # byebug
+    collection = Collection.all.find_all { |c| c.user_id == params[:id].to_i }
+  
+    if collection
+      result = collection.map do |c| 
+        { article: c.article }
+      end
+
+      render json: result
+    else
+      render json: { message: 'Could not find collection' }
+    end
+  end
+
   def create
     # byebug
     collection = Collection.find_or_create_by(collection_params)
