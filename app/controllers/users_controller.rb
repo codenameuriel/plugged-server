f9c8da1482ae6fe5928ed4e8c0001940fb2eddd9
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-06-06 22:22:04 -0400

    add logic to create a UserCategory

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,18 @@ class UsersController < ApplicationController
     # byebug
     user = User.find_by(username: params[:username])
     if user 
-      render json: user
+      render json: user, include: [:categories]
     else
       render json: { message: 'No user found by provided username'}
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
+    if user 
+      render json: user, include: [:categories]
+    else
+      render json: { message: 'Could not find user' }
     end
   end
 
@@ -13,7 +22,10 @@ class UsersController < ApplicationController
     # byebug
     user = User.find_or_create_by(user_params)
     if user
-      render json: user
+      params[:categories].each do |id|
+        user_category = UserCategory.create(user_id: user.id, category_id: id.to_i )
+      end
+      render json: user,  include: [:categories]
     else
       render json: { message: 'Could not create user' }
     end
